0938460e791006f01b0c453888d956894c3c6aa1
Davide Conti <dconti.dev@gmail.com>
2022-11-02 21:27:55 +0100

    Refactor to fix indentation

--- raindrops.sh
+++ raindrops.sh
@@ -2,29 +2,29 @@
 
 main () {
 
-string=""
+    string=""
 
-if (( "$1" % 3 == 0 ))
-then
- string+="Pling"
-fi
+    if (( "$1" % 3 == 0 ))
+    then
+        string+="Pling"
+    fi
 
-if (( "$1" % 5 == 0))
-then
-  string+="Plang"
-fi
+    if (( "$1" % 5 == 0))
+    then
+        string+="Plang"
+    fi
 
-if (( "$1" % 7 == 0))
-then
-  string+="Plong"
-fi
+    if (( "$1" % 7 == 0))
+    then
+        string+="Plong"
+    fi
 
-if [[ -n $string ]]
-then
-    echo "$string"
-else
-    echo "$1"
-fi
+    if [[ -n $string ]]
+    then
+        echo "$string"
+    else
+        echo "$1"
+    fi
 
 }
 
